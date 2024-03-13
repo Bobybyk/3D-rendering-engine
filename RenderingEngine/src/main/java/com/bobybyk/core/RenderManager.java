26537b9cc71d6cbfe73b206bd68c95c0e5d433b3
Bobybyk <matt.lf@proton.me>
2024-03-13 23:11:32 +0100

    shaders and colors

--- RenderingEngine/src/main/java/com/bobybyk/core/RenderManager.java
+++ RenderingEngine/src/main/java/com/bobybyk/core/RenderManager.java
@@ -1,6 +1,7 @@
 package com.bobybyk.core;
 
 import com.bobybyk.core.entity.Model;
+import com.bobybyk.core.utils.Utils;
 import com.bobybyk.test.Launcher;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
@@ -8,22 +9,28 @@ import org.lwjgl.opengl.GL30;
 
 public class RenderManager {
     private final WindowManager window;
+    private ShaderManager shader;
 
     public RenderManager() {
         window = Launcher.getWindow();
     }
 
     public void init() throws Exception {
-
+        shader = new ShaderManager();
+        shader.createVertexShader(Utils.loadRessource("/shaders/vertex.vs"));
+        shader.createFragmentShader(Utils.loadRessource("/shaders/fragment.fs"));
+        shader.link();
     }
 
     public void render(Model model) {
         clear();
+        shader.bind();
         GL30.glBindVertexArray(model.getId());
         GL20.glEnableVertexAttribArray(0);
         GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
         GL20.glDisableVertexAttribArray(0);
         GL30.glBindVertexArray(0);
+        shader.unbind();
     }
 
     public void clear() {
@@ -31,6 +38,6 @@ public class RenderManager {
     }
 
     public void cleanup() {
-
+        shader.cleanup();
     }
 }
